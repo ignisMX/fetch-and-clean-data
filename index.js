371b2f8d3f542ffc66c6ee7a113062a05302573e
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-02-21 23:37:33 -0600

    add basic function to read content of data

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const https = require('https');
 const url = 'https://coderbyte.com/api/challenges/json/age-counting';
 https.get(url, (response) => {
+    let body = '';
+    response.setEncoding('utf8');
+    response.on('data',(data) => {
+        body += data;
+    });
 
+    response.on('end',() => {
+        console.log(body);
+
+        return body;
+    });
 });
